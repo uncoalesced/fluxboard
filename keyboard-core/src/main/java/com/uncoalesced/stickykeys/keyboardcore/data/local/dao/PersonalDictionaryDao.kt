b04749fd3b6f6cf4b802dbd170be05028d3fe1a6
jvag15 <uncoalesced@gmail.com>
2026-07-27 21:57:05 +0530

    refactor(keyboard): modify data access object for personal dictionary

--- keyboard-core/src/main/java/com/uncoalesced/stickykeys/keyboardcore/data/local/dao/PersonalDictionaryDao.kt
+++ keyboard-core/src/main/java/com/uncoalesced/stickykeys/keyboardcore/data/local/dao/PersonalDictionaryDao.kt
@@ -6,13 +6,16 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.uncoalesced.stickykeys.keyboardcore.data.local.entity.PersonalWordEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PersonalDictionaryDao {
-
-    @Query("SELECT * FROM personal_dictionary WHERE word LIKE :prefix || '%' ORDER BY frequency DESC LIMIT :limit")
-    fun getSuggestionsForPrefix(prefix: String, limit: Int): List<PersonalWordEntity>
+    @Query(
+        "SELECT * FROM personal_dictionary WHERE word LIKE :prefix || '%' ORDER BY frequency DESC LIMIT :limit",
+    )
+    fun getSuggestionsForPrefix(
+        prefix: String,
+        limit: Int,
+    ): List<PersonalWordEntity>
 
     @Query("SELECT * FROM personal_dictionary WHERE word = :word LIMIT 1")
     fun getWord(word: String): PersonalWordEntity?
